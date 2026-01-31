8fb8db89087199578874a500c48cdfff76334b66
waku <wakunguma13@gmail.com>
2026-01-30 21:13:34 -0700

    Fix article sorting

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,26 @@
 import {type CollectionEntry, getCollection} from "astro:content";
 
+export interface GetPostsParams {
+    /**
+     * Sort the list in descending order
+     */
+    sort?: boolean
+}
+
 /**
  * Returns all the articles in the articles collection with the `preview`
  * field set to false.
  */
-export const getPosts = async (): Promise<CollectionEntry<"articles">[]> => {
+export const getPosts = async (params?: GetPostsParams): Promise<CollectionEntry<"articles">[]> => {
+    const sort = params?.sort ?? false;
     let articles = await getCollection("articles",(post) => {
         return !post.data.preview;
     });
 
-    articles = articles.sort((a,b) => a.data.published.getTime() + b.data.published.getTime());
+    if (sort){
+        articles = articles.sort((a,b) => a.data.published.getTime() - b.data.published.getTime()).reverse();
+        console.log("Sorting");
+    }
 
     return articles;
 };
