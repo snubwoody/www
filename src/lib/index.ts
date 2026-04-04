37c73868cbe5814d7979e60917a831c0e2066f01
Wakunguma K. <contact@wakunguma.com>
2026-04-03 19:35:35 -0600

    Add sortPosts function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,7 @@
 import {type CollectionEntry, getCollection} from "astro:content";
 
+export type SortOption = "title" | "date_created";
+
 export interface GetPostsParams {
     /**
      * Sort the list in descending order
@@ -22,5 +24,19 @@ export const getPosts = async (params?: GetPostsParams): Promise<CollectionEntry
     }
 
     return articles;
+};
+
+// TODO: test this
+export const sortPosts = (posts: CollectionEntry<"articles">[],opt: SortOption = "title"): CollectionEntry<"articles">[] => {
+    let sorted = posts;
+    switch (opt){
+        case "title":
+            sorted = posts.sort((a,b) => a.data.title.localeCompare(b.data.title));
+            break;
+            case "date_created":
+                sorted = posts.sort((a,b) => a.data.published.getTime() - b.data.published.getTime()).reverse();
+                break;
+    }
+    return sorted;
 };
 
